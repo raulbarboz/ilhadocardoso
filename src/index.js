770691007dcb0037830f8d5155ce641de394143a
raulbarboz <ruzito@gmail.com>
2019-06-17 13:35:18 -0300

    added middlaware req.url

--- src/index.js
+++ src/index.js
@@ -8,8 +8,22 @@ const hostname = process.env.HOST || 'localhost';
 app.use(express.static('public'))
 app.set('view engine', 'ejs');
 
+let url = null;
+
+app.use((req, res, next) => {
+    url = req.url;
+    if (url.substr(-1) === '/'){
+        url = url.substring(0, url.length -1);
+    }
+    next()
+})
+
 app.get('/maruja', (req, res) => {
-    res.render('comunidades')
+    res.render('comunidades', {url:url})
+})
+
+app.get('/maruja/praias', (req, res) => {
+    res.render('praias', {url : '/maruja'})
 })
 
 app.listen(port, hostname, () => {
