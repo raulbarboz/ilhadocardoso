5a9b271f82d008cbe4c95ecb8cfecb82ef5a81ac
raulbarboz <ruzito@gmail.com>
2019-06-17 14:51:02 -0300

    added json/readFileSync

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const ejs = require('ejs');
+const fs = require('fs');
 
 const port = process.env.PORT || 3000;
 const hostname = process.env.HOST || 'localhost';
@@ -8,22 +9,34 @@ const hostname = process.env.HOST || 'localhost';
 app.use(express.static('public'))
 app.set('view engine', 'ejs');
 
-let url = null;
+let url, data = null;
 
 app.use((req, res, next) => {
     url = req.url;
+    const rawData = fs.readFileSync('./src/data.json');
+    data = JSON.parse(rawData)
     if (url.substr(-1) === '/'){
         url = url.substring(0, url.length -1);
     }
     next()
 })
 
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
 app.get('/maruja', (req, res) => {
     res.render('comunidades', {url:url})
 })
 
+app.get('/baleia', (req, res) => {
+    res.render('comunidades', {url:url})
+})
+
 app.get('/maruja/praias', (req, res) => {
-    res.render('praias', {url : '/maruja'})
+    console.log(data["comunidades"]["maruja"]["praias"])
+    
+    res.render('praias', {url : '/maruja', data: data["comunidades"]["maruja"]["praias"]})
 })
 
 app.listen(port, hostname, () => {
